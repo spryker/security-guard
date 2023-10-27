2feebcb27f6ff7a8d1d3d859c9f2a304c8a8d0a6
Ruslan Dzhumaiev <ruslan.dzhumaiev@spryker.com>
2023-10-27 11:05:27 +0000

    API GET script to export data to the file

---
+++ get-a-whitelist.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -X GET -k "https://10.111.11.10/api/exception_lists/items/_find?list_id=6426f7c3-fc86-4a78-bbd5-4c8b3147aeee&per_page=90" \
+-u <user_name>:<password> \
+-H 'kbn-xsrf: true' \
+-H "Content-Type: application/json" \
+> report.json && less report.json | jq | grep 'spryker.com'
